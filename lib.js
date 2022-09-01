2a8cf53c921f00d02881145bee4be047703f4447
atharsh <atharshsivasamy99@gmail.com>
2022-09-01 21:32:53 +0530

    Atharsh added lib function

---
+++ lib.js
@@ -0,0 +1,16 @@
+//I90494
+module.exports.FizzBuzz = function (number) {
+    if(number%5==0 && number%3==0){
+        return "FizzBuzz";
+    }
+    else if(number%5==0){
+        return "Fizz";
+    }
+    else if(number%3==0){
+        return "Buzz";
+    }
+    else if(number%11==0){
+        return "BuzzBuzz"
+    }
+
+}
